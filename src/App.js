c591e6bd7936d30a0f628bb2a2a788728e379cb1
Mihisanka <mihisanka1998@gmail.com>
2023-07-28 02:04:26 +0530

    internal commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>SheyRooms</h1>
+      <Navbar/>
     </div>
   );
 }
